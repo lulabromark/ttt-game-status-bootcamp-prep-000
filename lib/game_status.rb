8858bfdd6f79eea703a7de24360d33bfa04164b9
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-18 03:09:17 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -53,9 +53,9 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board) && board.detect{|i| i == "O"}
-    return "O"
-  elsif won?(board) && board.detect{|i| i == "X"}
+  if won?(board) && board.detect{|i| i == "X"}
     return "X"
+  elsif won?(board) && board.detect{|i| i == "O"}
+    return "O"
   end
 end
